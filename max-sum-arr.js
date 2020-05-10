95a80ed0b494d43c4fe4437861773e03c3936f74
MCFrank16 <mecfrank16@gmail.com>
2020-05-10 21:29:20 +0200

    learning sliding window pattern

---
+++ max-sum-arr.js
@@ -0,0 +1,24 @@
+// this solution uses a method called sliding window pattern
+// where we loop through the arr once and then add the next value to 
+// the current temporaly sum without also forgetting to remove the first index at 0.
+
+// so that arrangement is what known as sliding window pattern, with a linear time complexity.
+
+function maxSumArr(arr, n){
+  let maxSum = 0;
+  let tempSum = 0;
+
+  for(let i = 0; i < n; i++){
+      maxSum += arr[i];
+  }
+
+  tempSum = maxSum;
+  for(let i = n; i < arr.length; i++){
+      tempSum += arr[i] - arr[i - n];
+      maxSum = Math.max(tempSum, maxSum);
+  }
+
+  return maxSum;
+}
+
+console.log(maxSumArr([2,6,9,2,1,8,5,6,3], 3))
