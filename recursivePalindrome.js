530a6e0cdf7a156d4544cfe53e9ee91021caab24
MCFrank16 <mecfrank16@gmail.com>
2020-05-16 16:02:30 +0200

    resolving isPalindrome with recursion

---
+++ recursivePalindrome.js
@@ -0,0 +1,10 @@
+function isPalindrome(str){
+  if(str.length === 1) return true;
+  if(str.length === 2) return str[0] === str[1];
+  if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1, -1));
+
+  return false;
+}
+
+console.log(isPalindrome("awesome"));
+console.log(isPalindrome("tacocat"));
