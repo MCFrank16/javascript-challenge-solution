d427687d2acdb7e587a38a18532fd0bcb2802e15
MCFrank16 <mecfrank16@gmail.com>
2020-05-17 20:53:47 +0200

    resolving stringify numbers in an object with recursion

---
+++ stringifyNumber.js
@@ -0,0 +1,25 @@
+function stringifyNumbers(obj){
+    let newObj = {};
+    for(let key in obj){
+        if(typeof obj[key] === 'number') {
+            newObj[key] = obj[key].toString();
+        } else if(typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+            newObj[key] = stringifyNumbers(obj[key]);
+        } else {
+            newObj[key] = obj[key];
+        }
+    }
+    return newObj;
+}
+let obj = { 
+    num : 1,
+    str: "stringy",
+    data: {
+        val: 4,
+        info: {
+            isRight: true,
+            random: 66
+        }
+    }
+}
+console.log(stringifyNumbers(obj));
