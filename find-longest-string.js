8dd0b84bc13f02926facb2a02b1e6bf99c83119d
MCFrank16 <boonatechnologies@gmail.com>
2020-09-20 17:41:08 +0200

    swap in place javascript

--- find-longest-string.js
+++ find-longest-string.js
@@ -3,46 +3,47 @@
 // // this function will return the longest substring with all distinct characters.
 // // if the string is empty or not provided return O.
 
-// function findLongestSubstring(str){
-//     // add whatever parameters you deem necessary - good luck!
+function findLongestSubstring(str){
+    // add whatever parameters you deem necessary - good luck!
 
-//     if(typeof(str) !== "string" || str.length === 0) return 0;
+  if(typeof(str) !== "string" || str.length === 0) return 0;
 
-//     let longest = 0;
-//   let seen = {};
-//   let start = 0;
+  let longest = 0;
+  let seen = {};
+  let start = 0;
  
-//   for (let i = 0; i < str.length; i++) {
+  for (let i = 0; i < str.length; i++) {
 
-//     let char = str[i];
+    let char = str[i];
 
-//     if (seen[char]) {
-//       start = Math.max(start, seen[char]);
-//     }
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
     
-//     longest = Math.max(longest, i - start + 1);
+    longest = Math.max(longest, i - start + 1);
     
-//     seen[char] = i + 1;
-//   }
-//   return longest;
-// }
-// console.log(findLongestSubstring("thisisawesome"));
+    seen[char] = i + 1;
+  }
+  console.log(seen);
+  return longest;
+}
+console.log(findLongestSubstring("abcabcbb"));
 
 // const countDown = (num) => (num === 1) ? 1 : num * countDown(num - 1);
 // console.log(countDown(5))
 
-function collectOdds(arr){
-    let newArr = [];
+// function collectOdds(arr){
+//     let newArr = [];
 
-    if(arr.length === 0){
-        return newArr;
-    }
+//     if(arr.length === 0){
+//         return newArr;
+//     }
 
-    if(arr[0] % 2 !== 0){
-        newArr.push(arr[0])
-    }
+//     if(arr[0] % 2 !== 0){
+//         newArr.push(arr[0])
+//     }
 
-    return newArr.concat(collectOdds(arr.slice(1)));
-}
+//     return newArr.concat(collectOdds(arr.slice(1)));
+// }
 
-console.log(collectOdds([1,2,3,4,5]))
+// console.log(collectOdds([1,2,3,4,5]))
